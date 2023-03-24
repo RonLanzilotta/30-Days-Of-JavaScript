c10eb92753ae19934a34a3cc13727b7887258751
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-03-24 18:56:24 -0400

    Day 3

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,2 +1,17 @@
 // this is your main.js script
 let challenge = '30 days of Javascript'
+
+let sentence = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let pattern = /\d+/g
+
+let income = sentence.match(pattern)
+
+function sum(arr) {
+    let sum = 0;
+    for (let i=0; i<arr.length; i++) {
+        sum = sum + parseInt(arr[i])
+        }
+    return sum;
+}
+
+console.log(sum(income))
